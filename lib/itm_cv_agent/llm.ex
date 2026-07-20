4cf96c2da2378ab1a4c2b0afc7f079ec591d6977
Jonas Nielsen <djonnneller@gmail.com>
2026-07-20 20:10:02 +0200

    Add tools

--- lib/itm_cv_agent/llm.ex
+++ lib/itm_cv_agent/llm.ex
@@ -1,5 +1,5 @@
 defmodule ItMinds.CvAgent.LLM do
-  alias ReqLLM.Context
+  alias ReqLLM.{Context, Tool}
 
   def model() do
     ReqLLM.model!(%{
@@ -20,4 +20,23 @@ defmodule ItMinds.CvAgent.LLM do
   @in_danish_prompt File.read!("lib/itm_cv_agent/llm/in-danish.md")
 
   def assistant_system_prompt(), do: @assistant_system_prompt <> @in_danish_prompt
+
+  @spec setup_tools() :: list(ReqLLM.Tool.t())
+  def setup_tools() do
+    [
+      write_project_experience_tool()
+    ]
+  end
+
+  @spec write_project_experience_tool() :: ReqLLM.Tool.t()
+  defp write_project_experience_tool() do
+    Tool.new!(
+      name: "get_weather",
+      description: "Get current weather for a location",
+      parameter_schema: [
+        location: [type: :string, required: true, doc: "City name"]
+      ],
+      callback: fn _args -> {:ok, %{temperature: 23, unit: "celsium"}} end
+    )
+  end
 end
