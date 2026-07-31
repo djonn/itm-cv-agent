64b46b558a07fed92ed240bd1545acd2cc136f19
Jonas Nielsen <djonnneller@gmail.com>
2026-07-31 17:20:20 +0200

    Implement translator state and tools

--- lib/itm_cv_agent/agents/translator.ex
+++ lib/itm_cv_agent/agents/translator.ex
@@ -1,7 +1,10 @@
 defmodule ItMinds.CvAgent.Agents.Translator do
-  alias ReqLLM.Context
+  alias ReqLLM.{Context, Tool}
+  alias ItMinds.CvAgent.ProjectExperience
 
   @behaviour ItMinds.CvAgent.Agents.Behaviour
+
+  @translatable_sections ~W"project_name project_description employee_role_name employee_role_description"
 
   @impl ItMinds.CvAgent.Agents.Behaviour
   def model() do
@@ -21,12 +24,67 @@ defmodule ItMinds.CvAgent.Agents.Translator do
 
   @impl ItMinds.CvAgent.Agents.Behaviour
   def new_state() do
-    nil
+    %{
+      "en" => %ProjectExperience{},
+      "da" => %ProjectExperience{}
+    }
   end
 
   @impl ItMinds.CvAgent.Agents.Behaviour
   def setup_tools(_state) do
-    []
+    [
+      write_english_translation(),
+      read_original_danish()
+    ]
+  end
+
+  defp write_english_translation() do
+    Tool.new!(
+      name: "write_english_translation",
+      description: "Write a segment of the project experience",
+      parameter_schema: [
+        section: [
+          type: {:in, @translatable_sections},
+          required: true,
+          doc: "What section to write to"
+        ],
+        value: [
+          type: :string,
+          required: true,
+          doc: "The new content of the section, everything in the section will be overridden"
+        ]
+      ],
+      callback: fn %{section: section, value: value} ->
+        state_updator = fn state ->
+          updated_translation =
+            state
+            |> Map.get("en")
+            |> Map.put(section |> String.to_atom(), value)
+
+          state |> Map.put("en", updated_translation)
+        end
+
+        {:set_state, "Succesfully updated translation", state_updator}
+      end
+    )
+  end
+
+  defp read_original_danish() do
+    Tool.new!(
+      name: "read_original_danish",
+      description: "Read a segment of the original danish project experience",
+      parameter_schema: [
+        section: [
+          type: {:in, @translatable_sections},
+          required: true,
+          doc: "What section to read"
+        ]
+      ],
+      callback: fn %{state: state, section: section} ->
+        section_content = state |> Map.get("da") |> Map.get(section |> String.to_atom(), "")
+        {:ok, section_content}
+      end
+    )
   end
 
   @translator_system_prompt File.read!("lib/itm_cv_agent/llm/translator-agent.md")
--- lib/itm_cv_agent/llm/translator-agent.md
+++ lib/itm_cv_agent/llm/translator-agent.md
@@ -4,4 +4,4 @@ Oversæt teksten en-til-en uden at omskrive noget.
 Oversæt ikke fagtermer hvor det ikke giver mening, fx. "Azure Functions" skal ikke oversættes.
 Oversæt ikke navne på firmaer, projekter, eller andre specifikke termer, fx. "AuctionConnect", "OnlineRekvisition", "Sundhed.dk"
 
-Du bør have fået en fil med en projekterfaring, som du skal oversætte.
+Du bør have fået en fil med en projekterfaring, som du skal oversætte. Tilgå hvert afsnit i filen via read_original_danish værktøjet og skriv oversættelsen med write_english_translation værktøjet.
